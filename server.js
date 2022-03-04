fe5f607c74086cd6799f4cf87d2f502e90b712ce
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-03 17:45:28 -0800

    removing brackets from one line functions

--- server.js
+++ server.js
@@ -101,16 +101,10 @@ app.delete('/api/notes/:id', (req, res) => {
     res.status(201).json(response) 
 })
 
-app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/notes.html'))
-})
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')))
 
 app.get('/api/notes', (req, res) => res.json(dataBase))
 
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public/index.html'))
-})
+app.get('*', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')))
 
-app.listen(PORT, () => {
-    console.log(`Example app listening at http://localhost:${PORT}`)
-})
+app.listen(PORT, () => console.log(`Example app listening at http://localhost:${PORT}`))
