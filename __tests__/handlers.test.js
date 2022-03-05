7e26cc582a977677faaf7bd1dcd7c6106719bb4f
Marcus Lewis <72634924+lewisemarcus@users.noreply.github.com>
2022-03-04 22:19:41 -0800

    added test suites for all pass cases for each http method

--- __tests__/handlers.test.js
+++ __tests__/handlers.test.js
@@ -1,20 +1,6 @@
 const handlers = require('../handlers/handlers')
 const supertest = require('supertest')
-const path = require('path')
-const { request } = require('http')
 const app = require('../app')
-
-describe('GET *', () => {
-    describe('Test the root path.', () => {
-        test(`The 'home' handler from 'handlers.js' should be called.`, () => {
-            const req = {}, res = { sendFile: jest.fn() }
-            const mock = jest.spyOn(handlers, 'home').mockImplementation(() => {})
-            handlers.home(req, res)
-            expect(mock).toHaveBeenCalled()
-            mock.mockRestore()
-        })
-    })
-})
 
 describe('POST /api/notes', () => {
     describe('Given a user enters a new note.', () => {
@@ -51,5 +37,89 @@ describe('POST /api/notes', () => {
             })
             expect(response.type).toBe(`application/json`)
          })
+    })
+})
+
+describe('DELETE /api/notes', () => {
+    describe('Test the delete function of the notes route', () => {
+        test(`The 'noteDelete' handler from 'handlers.js' should be called`, () => {
+            const req = {}, res = { sendFile: jest.fn() }
+            const mock = jest.spyOn(handlers, 'noteDelete').mockImplementation(() => {})
+            handlers.noteDelete(req, res)
+            expect(mock).toHaveBeenCalled()
+            mock.mockRestore()
+        })
+        test(`Expect DELETE request method`, async () => {
+            const response = await supertest(app).delete('/api/notes')
+            expect(response.req.method).toBe('DELETE')
+        })
+    })
+})
+
+describe('GET /notes', () => {
+    describe('Test the notes route.', () => {
+        test(`The 'notes' handler from 'handlers.js' should be called.`, () => {
+            const req = {}, res = { sendFile: jest.fn() }
+            const mock = jest.spyOn(handlers, 'notes').mockImplementation(() => {})
+            handlers.notes(req, res)
+            expect(mock).toHaveBeenCalled()
+            mock.mockRestore()
+        })
+        test(`Expect request method to be GET`, async () => {
+            const response = await supertest(app).get('/notes')
+            expect(response.req.method).toBe('GET')
+        })
+        test(`Expect status code 200`, async () => {
+            const response = await supertest(app).get('/notes')
+            expect(response.status).toBe(200)
+        })
+    })
+})
+
+describe('GET /api/notes', () => {
+    describe('Test the /api/notes path.', () => {
+        test(`The 'noteGet' handler from 'handlers.js' should be called.`, () => {
+            const req = {}, res = { sendFile: jest.fn() }
+            const mock = jest.spyOn(handlers, 'noteGet').mockImplementation(() => {})
+            handlers.noteGet(req, res)
+            expect(mock).toHaveBeenCalled()
+            mock.mockRestore()
+        })
+        test(`Expect request method to be GET`, async () => {
+            const response = await supertest(app).get('/api/notes')
+            expect(response.req.method).toBe('GET')
+        })
+        test(`Expect status code 200`, async () => {
+            const response = await supertest(app).get('/api/notes')
+            expect(response.status).toBe(201)
+        })
+        test(`Expect the response type to be json`, async () => {
+            const response = await supertest(app).get('/api/notes')
+            expect(response.type).toBe(`application/json`)
+        })
+        test(`Expect the body of the json file to contain an array`, async () => {
+            const response = await supertest(app).get('/api/notes')
+            expect(response._body.constructor).toBe(Array)
+        })
+    })
+})
+
+describe('GET *', () => {
+    describe('Test the root path.', () => {
+        test(`The 'home' handler from 'handlers.js' should be called.`, () => {
+            const req = {}, res = { sendFile: jest.fn() }
+            const mock = jest.spyOn(handlers, 'home').mockImplementation(() => {})
+            handlers.home(req, res)
+            expect(mock).toHaveBeenCalled()
+            mock.mockRestore()
+        })
+        test(`Expect request method to be GET`, async () => {
+            const response = await supertest(app).get('/')
+            expect(response.req.method).toBe('GET')
+        })
+        test(`Expect status code 200`, async () => {
+            const response = await supertest(app).get('/')
+            expect(response.status).toBe(200)
+        })
     })
 })
